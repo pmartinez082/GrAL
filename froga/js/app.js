4e21d294366df792e6a7d77be284b39d19f1f151
Paula <pmrtinez082@ikasle.ehu.eus>
2024-10-07 12:15:40 +0200

    20241007

--- froga/js/app.js
+++ froga/js/app.js
@@ -3,18 +3,27 @@ const izenaInput = document.getElementById("iz");
 const abizenakInput = document.getElementById("ab");
 const emaitza = document.getElementById("emaitza");
 const form1 = document.getElementById("form1")
-
+let galdera = document.getElementById("galdera");
+let hasiera = document.getElementById("hasiera");
 
 form1.addEventListener("submit", (event) =>{
     event.preventDefault();
     const izena = izenaInput.value;
     const abizenak = abizenakInput.value;
-    let galdera = document.getElementById("galdera");
+    
     galdera.setAttribute("hidden", "hidden");
     emaitza.innerHTML="izena: "+ izena+", abizenak: "+abizenak;
+    hasiera.removeAttribute("hidden");
     form1.reset();
     
 });
+
+function erakutsi(){
+    hasiera.setAttribute("hidden", "hidden");
+    galdera.removeAttribute("hidden");
+    emaitza.innerHTML = "";
+
+}
 
 
 
