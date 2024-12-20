a38d46a293a0c9a7907633f2543d35038814ee0f
Paula <pmrtinez082@ikasle.ehu.eus>
2024-12-20 12:04:56 +0100

    20241220

--- v3/app_admin/index.js
+++ v3/app_admin/index.js
@@ -9,10 +9,10 @@ import https from 'https';
 const app = express();
 
 //* Settings
-//const port = 443;
-//const host = '192.168.137.1'; 
-const port = 3000;
-const host = 'putxerapp.eus';
+const port = 443;
+const host = '192.168.137.1'; 
+//const port = 3000;
+//const host = 'putxerapp.eus';
 //* Middlewares
 app.use(morgan('dev'));
 
@@ -30,17 +30,17 @@ app.use(express.urlencoded( { extended:true }));
 //* Routes
 router(app);
 
-/*
-https.createServer({
-    cert: fs.readFileSync('https/cert.pem'),
-    key: fs.readFileSync('https/key.pem')
-  },app).listen(PUERTO, function(){
-     console.log('Server running at http://${host}:${port}');
- });
+const options = {
+    key: fs.readFileSync('https/key.pem'),
+    cert: fs.readFileSync('https/cert.pem')
+};
+
+const server = https.createServer(options, app);
  
-*/
+  
+
 
 //* Starting the server
-app.listen(port, host, () => {
+server.listen(port, host, () => {
     console.log(`Server running at http://${host}:${port}`);
 });
